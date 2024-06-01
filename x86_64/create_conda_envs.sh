e3fe22da7a9cfec96abee565d0f93e5c06fd3e8c
steepcurve <steepcurve@163.com>
2024-06-01 18:31:14 +0800

    lsp install

--- x86_64/create_conda_envs.sh
+++ x86_64/create_conda_envs.sh
@@ -10,8 +10,11 @@ source ~/.bashrc
 # Create and activate tf1.15 environment, then install packages
 conda create -n tf1 python=3.7.5 -y
 conda activate tf1
+python -m pip install -i https://pypi.tuna.tsinghua.edu.cn/simple --upgrade pip
+pip config set global.index-url https://pypi.tuna.tsinghua.edu.cn/simple
 # pip install tensorflow==1.15.0
-pip install -i https://pypi.tuna.tsinghua.edu.cn/simple tensorflow==1.15.0 numpy decorator sympy==1.4 cffi==1.12.3 pyyaml pathlib2 pandas grpcio grpcio-tools protobuf==3.20.0 scipy requests mpi4py easydict scikit-learn==0.20.0 attrs
+pip install tensorflow==1.15.0 numpy decorator sympy==1.4 cffi==1.12.3 pyyaml pathlib2 pandas grpcio grpcio-tools protobuf==3.20.0 scipy requests mpi4py easydict scikit-learn==0.20.0 attrs
+pip install pyright
 
 # Deactivate the environment
 conda deactivate
@@ -19,9 +22,12 @@ conda deactivate
 # Create and activate tf2.6 environment, then install packages
 conda create -n tf2 python=3.7.5 -y
 conda activate tf2
+python -m pip install -i https://pypi.tuna.tsinghua.edu.cn/simple --upgrade pip
+pip config set global.index-url https://pypi.tuna.tsinghua.edu.cn/simple
 # pip install tensorflow==2.6.5
-pip install -i https://pypi.tuna.tsinghua.edu.cn/simple tensorflow==2.6.5 numpy decorator sympy==1.4 cffi==1.12.3 pyyaml pathlib2 pandas grpcio grpcio-tools scipy requests mpi4py easydict scikit-learn==0.20.0 attrs
-pip install -i https://pypi.tuna.tsinghua.edu.cn/simple protobuf==3.20.0
+pip install tensorflow==2.6.5 numpy decorator sympy==1.4 cffi==1.12.3 pyyaml pathlib2 pandas grpcio grpcio-tools scipy requests mpi4py easydict scikit-learn==0.20.0 attrs
+pip install protobuf==3.20.0
+pip install pyright
 
 # Deactivate the environment
 conda deactivate
