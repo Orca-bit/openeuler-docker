cf4e53e449ed4d71dc3d771745e33b5f067789d4
steepcurve <steepcurve@163.com>
2024-06-06 11:09:11 +0800

    install tf2 h5py

--- aarch64/create_conda_tf2_envs.sh
+++ aarch64/create_conda_tf2_envs.sh
@@ -9,10 +9,12 @@ source ~/.bashrc
 conda create -n tf2 -c https://mirrors.tuna.tsinghua.edu.cn/anaconda/cloud/conda-forge python=3.7.5 -y
 
 wget --no-check-certificate -O /tmp/tensorflow-2.6.5-cp37-cp37m-manylinux2014_aarch64.whl https://ascend-repo.obs.cn-east-2.myhuaweicloud.com/MindX/OpenSource/python/packages/tensorflow-2.6.5-cp37-cp37m-manylinux2014_aarch64.whl#sha256=daf14490ef2b9334c7472d25ca66640e2546bc8e446c6bc1d88e5d4855e75db6
+wget --no-check-certificate /tmp/h5py-3.1.0-cp37-cp37m-manylinux2014_aarch64.whl https://ascend-repo.obs.cn-east-2.myhuaweicloud.com/MindX/OpenSource/python/packages/h5py-3.1.0-cp37-cp37m-manylinux2014_aarch64.whl#sha256=40dd06dce42fdb004b54a8f357ab6c181579320d410ad6ab0b0921f26df34f55
 
 # activate tf2.6 environment, then install packages
 conda activate tf2
 # pip install tensorflow==2.6.5
+pip install /tmp/h5py-3.1.0-cp37-cp37m-manylinux2014_aarch64.whl
 pip install /tmp/tensorflow-2.6.5-cp37-cp37m-manylinux2014_aarch64.whl
 pip install numpy decorator sympy==1.4 cffi==1.12.3 pyyaml pathlib2 pandas grpcio grpcio-tools protobuf==3.20.0 scipy requests mpi4py easydict scikit-learn==0.20.0 attrs
 pip install pyright
@@ -21,4 +23,4 @@ pip install pyright
 conda deactivate
 
 # rm tmp file
-rm /tmp/tensorflow*.whl -f
+rm /tmp/tensorflow*.whl h5py*.whl -f
