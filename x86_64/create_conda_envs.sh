4953d97b46ee328015d8432e3986f49671dd2ec9
steepcurve <steepcurve@163.com>
2024-06-17 20:58:20 +0800

    fix x86

--- x86_64/create_conda_envs.sh
+++ x86_64/create_conda_envs.sh
@@ -14,11 +14,11 @@ python -m pip install -i https://pypi.tuna.tsinghua.edu.cn/simple --upgrade pip
 pip config set global.index-url https://pypi.tuna.tsinghua.edu.cn/simple
 # pip install tensorflow==1.15.0
 pip install tensorflow==1.15.0 numpy decorator sympy==1.4 cffi==1.12.3 pyyaml pathlib2 pandas grpcio grpcio-tools protobuf==3.20.0 scipy requests mpi4py easydict scikit-learn==0.20.0 attrs
-pip install psutil==5.9
-pip install setuptools==41.2.0 wheel==0.40.0
+# pip install psutil==5.9
+# pip install setuptools==41.2.0 wheel==0.40.0
 HOROVOD_WITH_MPI=1 HOROVOD_WITH_TENSORFLOW=1 pip install horovod --no-cache-dir
-pip uninstall setuptools -y
-pip install setuptools
+# pip uninstall setuptools -y
+# pip install setuptools
 
 # Deactivate the environment
 conda deactivate
@@ -30,11 +30,11 @@ python -m pip install -i https://pypi.tuna.tsinghua.edu.cn/simple --upgrade pip
 pip config set global.index-url https://pypi.tuna.tsinghua.edu.cn/simple
 # pip install tensorflow==2.6.5
 pip install tensorflow==2.6.5 numpy decorator sympy==1.4 cffi==1.12.3 pyyaml pathlib2 pandas grpcio grpcio-tools protobuf scipy requests mpi4py easydict scikit-learn==0.20.0 attrs
-pip install psutil==5.9
-pip install setuptools==41.2.0 wheel==0.40.0
+# pip install psutil==5.9
+# pip install setuptools==41.2.0 wheel==0.40.0
 HOROVOD_WITH_MPI=1 HOROVOD_WITH_TENSORFLOW=1 pip install horovod --no-cache-dir
-pip uninstall setuptools -y
-pip install setuptools
+# pip uninstall setuptools -y
+# pip install setuptools
 
 # Deactivate the environment
 conda deactivate
